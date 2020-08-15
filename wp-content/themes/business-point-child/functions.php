94feb4abec08019ae439430f9757687c5ae4dcc3
dwkrupadhyay@gmail.com <root@Diwakars-MacBook-Pro.local>
2020-08-15 22:22:09 +0530

    increase file upload size

--- wp-content/themes/business-point-child/functions.php
+++ wp-content/themes/business-point-child/functions.php
@@ -1,6 +1,8 @@
 <?php
 //* Code goes here
-
+@ini_set( 'upload_max_size' , '64M' );
+@ini_set( 'post_max_size', '64M');
+@ini_set( 'max_execution_time', '300' );
 add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
 
 function enqueue_parent_styles() {

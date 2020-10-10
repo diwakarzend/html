5eea9ecb8a2a3086c0bf7fa55cdf7e436d432359
dwkrupadhyay@gmail.com <root@Diwakars-MacBook-Pro.local>
2020-10-11 00:31:46 +0530

    changes

--- wp-content/themes/business-point-child/functions.php
+++ wp-content/themes/business-point-child/functions.php
@@ -1,11 +1,11 @@
 <?php
 //* Code goes here
 
-add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
+add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles',20);
 
 function enqueue_parent_styles() {
     wp_register_style( 'parent-style', get_template_directory_uri().'/style.css' ,array(), rand(112,9998), 'all' );
-    wp_enqueue_style( 'parent-style');
+    wp_enqueue_style( 'parent-style' ,array(), rand(112,9998), 'all');
     
    }
 ?>

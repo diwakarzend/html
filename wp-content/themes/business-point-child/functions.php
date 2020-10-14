2d9127f81331ee585ce7654fd1be1c3b4fffbef9
dwkrupadhyay@gmail.com <root@Diwakars-MacBook-Pro.local>
2020-10-14 15:24:42 +0530

    version changes

--- wp-content/themes/business-point-child/functions.php
+++ wp-content/themes/business-point-child/functions.php
@@ -9,6 +9,4 @@ function enqueue_parent_styles() {
     
 wp_enqueue_style('main-styles', get_template_directory_uri() . '/css/style.css', array(), filemtime(get_template_directory() . '/css/style.css'), false);
    }
-
-   
 ?>

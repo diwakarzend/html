d99a07403576ccf831d10ca376f174a8d09d2407
dwkrupadhyay@gmail.com <root@Diwakars-MacBook-Pro.local>
2020-10-14 16:36:18 +0530

    version revert

--- wp-content/themes/business-point-child/functions.php
+++ wp-content/themes/business-point-child/functions.php
@@ -1,12 +1,9 @@
 <?php
 //* Code goes here
 
-add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles');
+add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
 
 function enqueue_parent_styles() {
-  // wp_enqueue_style( 'child-style', get_stylesheet_uri().'/style.css' ,array(), rand(112,9998), 'all' );
-   // wp_enqueue_style( 'parent-style' ,array(), rand(112,9998), 'all');
-    
-wp_enqueue_style('main-styles', get_template_directory_uri() . '/css/style.css', array(), filemtime(get_template_directory() . '/css/style.css'), false);
-   }
+   wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' ,array(), rand(111,9999), 'all' );
+}
 ?>

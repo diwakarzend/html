5b56cc1e8edb2aae3842103108b64ef7f0bec543
dwkrupadhyay@gmail.com <root@webetechies.com>
2020-12-13 12:13:55 +0000

    changes

--- wp-content/themes/business-point-child/functions.php
+++ wp-content/themes/business-point-child/functions.php
@@ -12,4 +12,14 @@ function my_custom_scriptss() {
   wp_enqueue_script( 'custom-js', get_stylesheet_directory_uri() . '/custom.js', array( 'jquery' ), rand(101,9999),true);
 }
 add_action( 'wp_enqueue_scripts', 'my_custom_scriptss' );
+
+function ssl_srcset( $sources ) {
+  foreach ( $sources as &$source ) {
+    $source['url'] = set_url_scheme( $source['url'], 'https' );
+  }
+
+  return $sources;
+}
+add_filter( 'wp_calculate_image_srcset', 'ssl_srcset' );
+
 ?>

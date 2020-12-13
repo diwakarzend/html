dd08eadffc037bd6f59abf2be837877d5b75f821
dwkrupadhyay@gmail.com <diwakarUpadhyay@Diwakars-MacBook-Pro.local>
2020-12-13 16:11:58 +0530

    changes

--- wp-config-prod.php
+++ wp-config-prod.php
@@ -26,7 +26,7 @@ define( 'DB_NAME', 'portfolio_wordpress' );
 define( 'DB_USER', 'root' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '12345678' );
+define( 'DB_PASSWORD', '123456' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );

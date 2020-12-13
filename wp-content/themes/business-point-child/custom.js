b7a487b769ba1b78dab94c5fbd88dc58f09c30fa
dwkrupadhyay@gmail.com <root@Diwakars-MacBook-Pro.local>
2020-12-13 15:12:37 +0530

    changes

--- wp-content/themes/business-point-child/custom.js
+++ wp-content/themes/business-point-child/custom.js
@@ -8,3 +8,11 @@ function myFunction() {
     header.classList.remove("header-sticky");
   }
 }
+jQuery(".wpcf7-submit").click(function(){
+  console.log(jQuery(".wpcf7-form").hasClass("invalid"));
+  setTimeout(function(){
+  if(jQuery(".wpcf7-form").hasClass("invalid")){
+  jQuery(".wpcf7-form").scrollTop(300); 
+  }
+  },7000);
+  });

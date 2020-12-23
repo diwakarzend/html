2dc1727db814968d26ac8bca15aa09b47f207012
dwkrupadhyay@gmail.com <diwakarUpadhyay@Diwakars-MacBook-Pro.local>
2020-12-23 14:17:41 +0530

    update changes

--- wp-content/themes/business-point-child/header.php
+++ wp-content/themes/business-point-child/header.php
@@ -28,6 +28,16 @@ do_action( 'business_point_doctype' );
 do_action( 'business_point_head' );
 
 wp_head(); ?>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-23664067-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-23664067-1');
+</script>
+
 </head>
 
 <body <?php body_class(); ?>>
